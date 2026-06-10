f46ff6ab42bb37fbd1ebdb45a1dd677680006de7
Diversworld <eckhard@diversworld.eu>
2026-06-10 15:55:36 +0200

    Update route resolver calls to include 'attribute' parameter
    Adjusted the `resolve` and `load` method calls in multiple Plugin files to incorporate the 'attribute' parameter. This change ensures consistency in route handling and aligns with updated standards for controller resolution.

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -41,7 +41,6 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface
     {
         return $resolver
             ->resolve(__DIR__ . '/../Controller', 'attribute')
-            ->load(__DIR__ . '/../Controller/Api', 'attribute')
-        ;
+            ->load(__DIR__ . '/../Controller', 'attribute');
     }
 }
